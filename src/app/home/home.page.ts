990bd06096a9df867d45cb9c86ee43135f56fc4e
iwp <email@domain.de>
2022-11-03 21:36:14 +0100

    first Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  currentNumber = '0';
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public getNumber(v: string){
+    console.log(v);
+    if(this.waitForSecondNumber)
+    {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    }else{
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+    }
+  }
+
+  getDecimal(): void{
+    if(!this.currentNumber.includes('.')){
+        this.currentNumber += '.';
+    }
+  }
+
+  private doCalculation(op, secondOp){
+    switch (op){
+      case '+':
+      return this.firstOperand += secondOp;
+      case '-':
+      return this.firstOperand -= secondOp;
+      case '*':
+      return this.firstOperand *= secondOp;
+      case '/':
+      return this.firstOperand /= secondOp;
+      case '=':
+      return secondOp;
+    }
+  }
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public getOperation(op: string){
+    console.log(op);
+    if(this.firstOperand === null){
+      this.firstOperand = Number(this.currentNumber);
+
+    }else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber));
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public clear(){
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
 }
